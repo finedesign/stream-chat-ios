a8523f9eb8848a106b10ba3a9beb800dca883bf4
PR <finedesign@gmail.com>
2025-05-16 17:01:47 -0700

    Initial Xcode project setup

---
+++ StreamChatDemo/StreamChatDemo/StreamChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamChatDemoApp.swift
+//  StreamChatDemo
+//
+//  Created by Paul Reid on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct StreamChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StreamChatDemo/StreamChatDemoTests/StreamChatDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  StreamChatDemoTests.swift
+//  StreamChatDemoTests
+//
+//  Created by Paul Reid on 5/16/25.
+//
+
+import Testing
+@testable import StreamChatDemo
+
+struct StreamChatDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
